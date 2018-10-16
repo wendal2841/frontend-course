73f954ecc39c780771b8af0a491acf792d92a6f6
GruzinskiyAl <gruzinskiy.al@gmail.com>
2018-10-17 01:19:16 +0300

    first task

--- homework_1/script.js
+++ homework_1/script.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 
-let userValueId = {};
+let taskIdUserValue = {};
 for (let i=1; i<=30; i++){
-    userValueId[i] = ['task'+i+'_value1', 'task'+i+'_value2', 'task'+i+'_value3'];
+    taskIdUserValue[i] = ['task'+i+'_value1', 'task'+i+'_value2', 'task'+i+'_value3'];
 }
 
 let results = {};
@@ -10,21 +10,39 @@ for (let i=1; i<=30; i++){
     results[i] = 'task'+i+'Result';
 }
 
-let getUserValue = (inputId) => {
-    return document.getElementById(inputId).value;
+class TaskExecutor {
+    constructor(id){
+        this.taskId = id;
+    }
+
+    getUserValueList () {
+        let result = [];
+        for(let i=0; i<taskIdUserValue[+this.taskId].length; i++){
+            let value = this.getUserValue(taskIdUserValue[this.taskId][i]);
+            if (value) result.push(value);
+        }
+        return result;
+    }
+
+    getUserValue(inputId) {
+        try {
+            return document.getElementById(inputId).value;
+        } catch (TypeError) {
+            return null;
+        }
+    }
+
+    printResult(taskResult) {
+        document.getElementById(results[this.taskId]).innerHTML = taskResult;
+    }
 };
 
-let taskFunctions = {};
-
-let printResult = (taskId) => {
-    let methodName = "task" + taskId;
-    document.getElementById(results[taskId]).innerHTML = taskFunctions[methodName](taskId);
+let task1 = (taskId) => {
+    let executor = new TaskExecutor(taskId);
+    let userValues = executor.getUserValueList();
+    let a = +userValues[0];
+    let b = +userValues[1];
+    let result = (a % 2 !== 0) ? a * b : a + b;
+    executor.printResult(result.toString());
 };
-
-let taskFunctions[task1] = (taskId) => {
-    a = getUserValue(userValueId[taskId][0]);
-    b = getUserValue(userValueId[taskId][1]);
-    return (a % 2 !== 0)? a*b: a+b;
-};
-
 
