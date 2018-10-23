74de664545f5d9fab129a1ac865f62c736fb94bd
GruzinskiyAl <gruzinskiy.al@gmail.com>
2018-10-24 01:39:01 +0300

    new commit

--- homework_1/calculator_script.js
+++ homework_1/calculator_script.js
@@ -1,208 +1,80 @@
-'use strict';
 
-class Calculator{
-    constructor(){
-        this.x = null;
-        this.y = null;
-        this.result = 0
+var queue = [];
+var input = 0;
+function calculateQueue(value){
+    if (input !==0) {
+        input = parseFloat(input);
+
+        addToQueue(input);
     }
+    var answer = value[0];
+    var dividedByZero = 0;
+    for (  var i = 2; i < value.length; i= i+2) {
 
-    clear(){
-        this.x = null;
-        this.y = null;
-        this.result = 0;
-    }
+        switch (queue[i-1]) {
+            case '+':
+                answer+= value[i];
+                break;
+            case '-':
+                answer-= value[i];
+                break;
+            case '/':    if (value[i] === 0)   dividedByZero = 1;
+            else      answer = answer / value[i];
 
-    action(operation, value){
-        if(this.x === null){
-            this.x = Math.abs(+value);
-        } else if (this.x !== null && this.y === null && operation) {
-            this.y = Math.abs(+value);
-            this.calculate(operation)
-        }
-    }
-
-    getResult(){
-        this.x = null;
-        this.y = null;
-        return this.result;
-    }
-
-    calculate(action){
-        switch (action) {
-            case 'minus':
-                this.result = this.x - this.y;
                 break;
-            case 'plus':
-                this.result = this.x + this.y;
-                break;
-            case 'multiplication':
-                this.result = this.x * this.y;
-                break;
-            case 'division':
-                this.result = this.x / this.y;
-                break;
-            case 'sin':
-                this.field.value = '-';
-                break;
-            case 'cos':
-                this.field.value = '+';
-                break;
-            case 'tan':
-                this.field.value = '*';
-                break;
-            case 'log':
-                this.field.value = '/';
-                break;
-            case 'ln':
-                this.field.value = '-';
-                break;
-            case 'exp':
-                this.field.value = '+';
-                break;
-            case 'sqr':
-                this.field.value = '*';
-                break;
-            case 'pow':
-                this.field.value = '-';
-                break;
-            case 'sqrt':
-                this.field.value = '+';
-                break;
-            case 'percent':
-                this.field.value = '*';
-                break;
-            case 'pi':
-                this.field.value = '/';
+            case'*': answer = answer * value[i];
                 break;
         }
-        this.x = this.result;
-        this.y = null;
+
     }
 
+    answer = answer.toFixed(10);
+    answer = parseFloat(answer);
+    if ( dividedByZero === 1) { clearAll();
+        document.getElementById("answer").innerHTML =  "ERROR";}
+    else
+    {document.getElementById("answer").innerHTML =  answer ;
+        input = answer;
+        queue = [];}
 
 
 }
-
-class Controller{
-    constructor(){
-        this.calculator = new Calculator();
-        this.curentOperation = null;
-    }
-
-    getField() {
-        this.field = document.getElementById('field');
-    }
-
-    printDot(){
-        if(this.field.value.indexOf('.') === -1) this.field.value += '.'
-    }
-
-    printNumber(number) {
-        if(('*/').indexOf(this.field.value) !== -1) this.field.value = number.toString();
-        else this.field.value += number.toString();
-    }
-
-    printResult(){
-        this.field.value = this.calculator.getResult()
-    }
-
-    printOperation(action) {
-        switch (action) {
-            case 'minus':
-                this.field.value = '-';
-                break;
-            case 'plus':
-                this.field.value = '+';
-                break;
-            case 'multiplication':
-                this.field.value = '*';
-                break;
-            case 'division':
-                this.field.value = '/';
-                break;
-            case 'sin':
-                this.field.value = 'sin ';
-                break;
-            case 'cos':
-                this.field.value = 'cos ';
-                break;
-            case 'tan':
-                this.field.value = 'tan ';
-                break;
-            case 'log':
-                this.field.value = 'log ';
-                break;
-            case 'ln':
-                this.field.value = 'ln';
-                break;
-            case 'exp':
-                this.field.value = 'exp ';
-                break;
-            case 'sqr':
-                this.field.value = '^2';
-                break;
-            case 'pow':
-                this.field.value = '^';
-                break;
-            case 'sqrt':
-                this.field.value = '√';
-                break;
-            case 'percent':
-                this.field.value = '%';
-                break;
-            case 'pi':
-                this.field.value = 'pi';
-                break;
-        }
-        this.curentOperation = action;
-    }
-
-    calculate() {
-        if (+this.field.value)
-        this.calculator.action(this.curentOperation, this.field.value)
-    }
-
-    backspace(){
-        this.field.value = this.field.value.slice(0, -1)
-    }
-
-    clear(){
-        this.field.value = '';
-        this.curentOperation = null;
-        this.calculator.clear();
-    }
+function addToQueue(input){
+    queue.push(input);
 }
+function clearAll() {
+    queue = [];
+    input = 0;
+    document.getElementById("answer").innerHTML = "0";
+}
+function numericButton(arg){
+    if ( document.getElementById("answer").innerHTML ===  "ERROR" || (document.getElementById("answer").innerHTML == "0" && arg != "."))
+    { document.getElementById("answer").innerHTML = ""; }
 
-let controller = new Controller();
+    if (!(arg === ".") || !input.match(/[.]/)) {
 
-let numberAction = (number) => {
-    controller.getField();
-    controller.printNumber(number);
-};
+        input += arg;
+        document.getElementById("answer").innerHTML += arg;}
 
-let operationAction = (action) => {
-    controller.getField();
-    controller.calculate();
-    controller.printOperation(action);
 
-};
+}
+function operatorButton(arg){
+    if (input !== 0 && input !== "-") {
+        input = parseFloat(input);
+        addToQueue(input);
+        addToQueue(arg);
+        document.getElementById("answer").innerHTML +=arg;
+        input = 0;
 
-let dotAction = () => {
-    controller.printDot()
-};
+    }
+    if (arg == "-"  && isNaN(queue[0]) && input !== "-")
+    {input ="-";
 
-let calculate = () => {
-    controller.calculate();
-    controller.printResult()
-};
+        document.getElementById("answer").innerHTML = "-";
 
-let backspaceAction = () => {
-    controller.getField();
-    controller.backspace();
-};
 
-let clearAll = () => {
-    controller.getField();
-    controller.clear();
-};
+    }
+
+
+}
+       
