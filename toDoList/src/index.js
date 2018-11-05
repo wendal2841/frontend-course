a3e9a20f17babb4918a7684c42463f2124d772bf
alexandr.gruzinskiy <alexandr.gruzinskiy@eosda.com>
2018-11-05 18:25:33 +0200

    paint app tabs

--- toDoList/src/index.js
+++ toDoList/src/index.js
@@ -1,76 +1,76 @@
 "use strict";
 
-const taskList = document.getElementById('taskList');
-const taskCreateContainer = document.getElementById('taskCreateContainer');
+var taskList = document.getElementById("taskList");
+var taskCreateContainer = document.getElementById("taskCreateContainer");
 
 function callFiedl() {
-    taskCreateContainer.style.display = 'flex';
-};
+	taskCreateContainer.style.display = "flex";
+}
 
 function callAddTask() {
-    let field = document.getElementById('taskCreateField');
-    if(field.value === '') {
-        callTextFieldError(field);
-     } else {
-        addTask(field.value);
-        taskCreateContainer.style.display = 'none';
-        prepareField(field);
-    };
+	var field = document.getElementById("taskCreateField");
+	if(field.value === "") {
+		callTextFieldError(field);
+	} else {
+		addTask(field.value);
+		taskCreateContainer.style.display = "none";
+		prepareField(field);
+	}
 }
 
 function closeField() {
-    let field = document.getElementById('taskCreateField');
-    prepareField(field);
-    taskCreateContainer.style.display = 'none';
+	var field = document.getElementById("taskCreateField");
+	prepareField(field);
+	taskCreateContainer.style.display = "none";
 }
 
 function prepareField(field) {
-    field.value = '';
-    field.style.border = '1px solid black';
+	field.value = "";
+	field.style.border = "1px solid black";
 }
 
 function callTextFieldError(field) {
-    field.style.border = '1px solid red';
+	field.style.border = "1px solid red";
 }
 
 
 function setDeleteParentOnClick(element) {
-    element.onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
-};
-
-function createDeleteButton(parent) {
-    let span = document.createElement("span");
-    var text = document.createTextNode("\u00D7");
-    span.className = "delete-task";
-    span.appendChild(text);
-    parent.appendChild(span);
-    setDeleteParentOnClick(span);
+	element.onclick = function() {
+		var div = this.parentElement;
+		div.style.display = "none";
+	};
 }
 
-for (let i = 0; i < taskList.children.length; i++){
-    createDeleteButton(taskList.children[i]);
+function createDeleteButton(parent) {
+	var span = document.createElement("span");
+	var text = document.createTextNode("\u00D7");
+	span.className = "delete-task";
+	span.appendChild(text);
+	parent.appendChild(span);
+	setDeleteParentOnClick(span);
+}
+
+for (var i = 0; i < taskList.children.length; i++){
+	createDeleteButton(taskList.children[i]);
 }
 
 function addTask(string) {
-    let task = document.createElement('li');
-    let text = document.createTextNode(string);
+	var task = document.createElement("li");
+	var text = document.createTextNode(string);
 
-    task.appendChild(text);
-    createDeleteButton(task);
+	task.appendChild(text);
+	createDeleteButton(task);
     
-    taskList.appendChild(task);
-};
+	taskList.appendChild(task);
+}
 
-var list = document.getElementById('taskList');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
+var list = document.getElementById("taskList");
+list.addEventListener("click", function(ev) {
+	if (ev.target.tagName === "LI") {
+		ev.target.classList.toggle("checked");
+	}
 }, false);
 
 module.exports.default = {
-    addTask: addTask,
-}
+	addTask: addTask,
+};
